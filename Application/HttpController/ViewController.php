780765483341b4826cd40ae73bba766484167306
liuyan <1260192349@qq.com>
2018-03-29 12:25:20 +0800

    test

--- Application/HttpController/ViewController.php
+++ Application/HttpController/ViewController.php
@@ -3,7 +3,7 @@
 namespace App\HttpController;
 
 use EasySwoole\Core\Http\AbstractInterface\Controller;
-use Illuminate\Support\Facades\Blade;
+use Jenssegers\Blade\Blade;
 
 abstract class ViewController extends Controller{
 
