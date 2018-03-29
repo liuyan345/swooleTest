09ee8eea7da646aeee42ecbdc51504b0f81d2789
liuyan <1260192349@qq.com>
2018-03-29 15:11:36 +0800

    test

--- Application/ViewController.php
+++ Application/ViewController.php
@@ -1,12 +1,12 @@
 <?php
 namespace App;
 
+use duncan3dc\blade\src\BladeInstance;
 use EasySwoole\Core\Component\Di;
 use EasySwoole\Core\Component\SysConst;
 use EasySwoole\Core\Http\AbstractInterface\Controller;
 use EasySwoole\Core\Http\Request;
 use EasySwoole\Core\Http\Response;
-use duncan3dc\Laravel\BladeInstance;
 
 /**
  * 视图控制器
@@ -27,6 +27,7 @@ abstract class ViewController extends Controller
      */
     function __construct( $actionName, Request $request, Response $response)
     {
+
         $tempPath   = Di::getInstance()->get(SysConst::DIR_TEMP);    # 临时文件目录
         $this->view = new BladeInstance(EASYSWOOLE_ROOT . '/Views', "{$tempPath}/templates_c");
 
