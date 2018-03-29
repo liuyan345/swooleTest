cbcd8a873cdad12ff59d04af1b3dcf8c08ea24d5
liuyan <1260192349@qq.com>
2018-03-29 14:36:33 +0800

    test

--- Application/HttpController/Index.php
+++ Application/HttpController/Index.php
@@ -4,13 +4,22 @@ namespace App\HttpController;
 
 //use EasySwoole\Core\Http\AbstractInterface\Controller;
 use App\ViewController;
+use duncan3dc\Laravel\BladeInstance;
+use EasySwoole\Core\Component\Di;
+use EasySwoole\Core\Component\SysConst;
 
 class Index extends ViewController
 {
+    protected $view;
     public function index()
     {
-        $this->view("Index/index",['name'=>"easySwoole"]);
-//        $this->response()->write('Hello easySwoole!');
+        $tempPath   = Di::getInstance()->get(SysConst::DIR_TEMP);    # 临时文件目录
+        $this->view = new BladeInstance(EASYSWOOLE_ROOT . '/Views', "{$tempPath}/templates_c");
+        $content = $this->view->render("Index/index",['name'=>"easySwoole"]);
+        $this->response()->write($content);
+
+        //        $this->response()->write('Hello easySwoole!');
+
     }
     public function test(){
         $this->response()->write('this is test route!');
