2f4f0300fd3882b0de16ae31a721f34fbaea2124
liuyan <1260192349@qq.com>
2018-03-29 14:47:36 +0800

    test

--- Application/HttpController/Index.php
+++ Application/HttpController/Index.php
@@ -2,24 +2,25 @@
 
 namespace App\HttpController;
 
-//use EasySwoole\Core\Http\AbstractInterface\Controller;
-use App\ViewController;
-use duncan3dc\Laravel\BladeInstance;
-use EasySwoole\Core\Component\Di;
-use EasySwoole\Core\Component\SysConst;
+use EasySwoole\Core\Http\AbstractInterface\Controller;
+//use App\ViewController;
+//use duncan3dc\Laravel\BladeInstance;
+//use EasySwoole\Core\Component\Di;
+//use EasySwoole\Core\Component\SysConst;
 
-class Index extends ViewController
+//class Index extends ViewController
+class Index extends Controller
 {
     protected $view;
     public function index()
     {
-        echo 1111;die;
-        $tempPath   = Di::getInstance()->get(SysConst::DIR_TEMP);    # 临时文件目录
-        $this->view = new BladeInstance(EASYSWOOLE_ROOT . '/Views', "{$tempPath}/templates_c");
-        $content = $this->view->render("Index/index",['name'=>"easySwoole"]);
-        $this->response()->write($content);
+//        echo 1111;die;
+//        $tempPath   = Di::getInstance()->get(SysConst::DIR_TEMP);    # 临时文件目录
+//        $this->view = new BladeInstance(EASYSWOOLE_ROOT . '/Views', "{$tempPath}/templates_c");
+//        $content = $this->view->render("Index/index",['name'=>"easySwoole"]);
+//        $this->response()->write($content);
 
-        //        $this->response()->write('Hello easySwoole!');
+                $this->response()->write('Hello easySwoole!!');
 
     }
     public function test(){
