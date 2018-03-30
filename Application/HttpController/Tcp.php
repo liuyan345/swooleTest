6406022c681ccb4590b15b35d62e9ec8eaee545f
liuyan <1260192349@qq.com>
2018-03-30 11:00:29 +0800

    test

--- Application/HttpController/Tcp.php
+++ Application/HttpController/Tcp.php
@@ -7,11 +7,10 @@ use EasySwoole\Core\Swoole\ServerManager;
 class Tcp extends Controller{
 
     function index(){
-        $this->actionNotFound(null);
+//        $this->actionNotFound(null);
     }
 
     function push(){
-        echo 1;die;
         $fd = intval($this->request()->getRequestParam('fd'));
         $info = ServerManager::getInstance()->getServer()->connection_info($fd);
         if(is_array($info)){
