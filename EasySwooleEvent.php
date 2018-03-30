fd54e5f50b6dfc711343cb4b8087c96addf7497a
liuyan <1260192349@qq.com>
2018-03-30 10:31:42 +0800

    test

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -15,6 +15,7 @@ use \EasySwoole\Core\Swoole\ServerManager;
 use \EasySwoole\Core\Swoole\EventRegister;
 use \EasySwoole\Core\Http\Request;
 use \EasySwoole\Core\Http\Response;
+use EasySwoole\Core\Swoole\Task\TaskManager;
 
 Class EasySwooleEvent implements EventInterface {
 
@@ -27,7 +28,16 @@ Class EasySwooleEvent implements EventInterface {
     public function mainServerCreate(ServerManager $server,EventRegister $register): void
     {
         // TODO: Implement mainServerCreate() method.
-        EventHelper::registerDefaultOnMessage($register,new Parser());
+//        EventHelper::registerDefaultOnMessage($register,new Parser());
+        $tcp = $server->addServer("tcp",9502);
+        EventHelper::registerDefaultOnReceive($register,new \Tcp\Parser(),function($errorType,$clientData,$client){
+            TaskManager::async(function() use($client){
+                sleep(3);
+                \EasySwoole\Core\Socket\Response::response($client,"Bye");
+                ServerManager::getInstance()->getServer()->close($client->getFd());
+            });
+            return $errorType." and going to close";
+        });
     }
 
     public function onRequest(Request $request,Response $response): void
---
+++ Tcp/Parser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tcp;
+
+use EasySwoole\Core\Socket\AbstractInterface\ParserInterface;
+use EasySwoole\Core\Socket\Common\CommandBean;
+
+class Parser implements ParserInterface
+{
+    public function decode($raw, $client): ?CommandBean
+    {
+        // TODO: Implement decode() method.
+        $list = explode(":",trim($raw));
+        $bean = new CommandBean();
+        $controller = array_shift($list);
+        if($controller == 'test'){
+            $bean->setControllerClass(Test::class);
+        }
+        $bean->setAction(array_shift($list));
+        $bean->setArg('test',array_shift($list));
+        return $bean;
+    }
+
+    public function encode(string $raw, $client, $commandBean):?string
+    {
+        // TODO: Implement encode() method.
+        return $raw."\n";
+
+    }
+
+
+}
+
+
+?>
